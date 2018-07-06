10bc6d0cc1b0a5233c0e213bd26ee0de486cbfc0
ivantiu <ivankentiu@gmail.com>
2018-07-06 08:01:42 +0800

    GameMode Done

--- SHealthComponent.cpp
+++ SHealthComponent.cpp
@@ -3,12 +3,14 @@
 #include "SHealthComponent.h"
 #include "Net/UnrealNetwork.h"
 #include <GameFramework/Actor.h>
+#include "SGameMode.h"
 
 // Sets default values for this component's properties
 USHealthComponent::USHealthComponent()
 {
 
 	DefaultHealth = 100;
+	bIsDead = false;
 
 	SetIsReplicated(true);
 }
@@ -42,7 +44,7 @@ void USHealthComponent::OnRep_Health(float OldHealth)
 
 void USHealthComponent::HandleTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
 {
-	if (Damage <= 0.0f)
+	if (Damage <= 0.0f || bIsDead)
 	{
 		return;
 	}
@@ -52,7 +54,23 @@ void USHealthComponent::HandleTakeAnyDamage(AActor* DamagedActor, float Damage, 
 
 	UE_LOG(LogTemp, Warning, TEXT("Health Changed: %s"), *FString::SanitizeFloat(Health));
 
+	bIsDead = Health <= 0.0f;
+
 	OnHealthChanged.Broadcast(this, Health, Damage, DamageType, InstigatedBy, DamageCauser);
+
+	if (bIsDead)
+	{
+		ASGameMode* GM = Cast<ASGameMode>(GetWorld()->GetAuthGameMode());
+		if (GM)
+		{
+			GM->OnActorKilled.Broadcast(GetOwner(), DamageCauser, InstigatedBy);
+		}
+	}
+}
+
+float USHealthComponent::GetHealth() const
+{
+	return Health;
 }
 
 void USHealthComponent::Heal(float HealAmount)
